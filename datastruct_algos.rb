bdda9a1fda2848bc78970d16e9ebcdae528ed331
Your Nameandrewyoo94 <andrewyoo.ny@gmail.com>
2021-07-26 21:47:38 -0400

    Fibonacci

--- datastruct_algos.rb
+++ datastruct_algos.rb
@@ -79,3 +79,16 @@ def binary_search (arr,key)
     
     return "Value not found in array!"
 end 
+
+# Fibonacci
+
+def fib(n)
+  first_num, second_num = [0, 1]
+  (n - 1).times do
+    first_num, second_num = second_num, first_num + second_num
+    
+    # first_num = second_num
+    # second_num = first_num + second_num
+  end
+  puts first_num
+end
