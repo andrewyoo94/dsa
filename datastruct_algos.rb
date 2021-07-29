ae188a7d930bdbca50f72550aae654f0697325db
Your Nameandrewyoo94 <andrewyoo.ny@gmail.com>
2021-07-28 23:20:36 -0400

    Bubble Sort

--- datastruct_algos.rb
+++ datastruct_algos.rb
@@ -100,3 +100,21 @@ def reverse_string(str)
   reversed = []
   str.size.times { reversed << split_str.pop }
 end
+
+# Bubble Sort
+
+def bubble_sort(a)
+  n = a.length
+  
+  for i in 0...n - 1
+   for j in 0...n - i-1
+       if a[j] > a[j+1]
+           temp=a[j]
+           a[j]=a[j+1]
+           a[j+1]=temp
+       end
+   end
+  end
+
+  return a
+end
