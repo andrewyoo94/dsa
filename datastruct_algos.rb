b4f412df598ff8b33563e7921acb84ec2cc6bc15
Your Nameandrewyoo94 <andrewyoo.ny@gmail.com>
2021-08-13 22:35:36 -0400

    Insertion Sort

--- datastruct_algos.rb
+++ datastruct_algos.rb
@@ -328,3 +328,25 @@ def selection_sort(a)
 
  return a
 end
+
+
+# Insertion Sort
+
+
+def insertion_sort(a)
+  for i in 1...(a.length)
+      j=i
+      while j>0
+          if a[j-1]>a[j]
+              temp=a[j]
+              a[j]=a[j-1]
+              a[j-1]=temp
+          else
+            break
+          end
+          j=j-1
+      end
+  end
+  return a
+
+end
