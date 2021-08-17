5f6ef1e0700f82d3f3872c55c1462a1ea16ba7f9
Your Nameandrewyoo94 <andrewyoo.ny@gmail.com>
2021-08-17 18:32:09 -0400

    Factorial

--- datastruct_algos.rb
+++ datastruct_algos.rb
@@ -371,3 +371,22 @@ def binary_search(array, key)
   
   nil
 end
+
+
+# Factorial Counter
+
+def fact(n)
+  num=0
+  len=Math.sqrt(n).floor
+  for i in 1..len
+     if n%i==0
+         if n/i!=i
+             num+=2
+           else
+               num+=1
+           end
+       end
+   end
+   
+   return num
+end
