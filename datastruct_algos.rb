5c172c7616b63b973c215b170245dad417092f8d
Your Nameandrewyoo94 <andrewyoo.ny@gmail.com>
2021-08-20 15:34:29 -0400

    Quick Sort

--- datastruct_algos.rb
+++ datastruct_algos.rb
@@ -422,3 +422,18 @@ def max_sum(a)
 
 	return max_so_far
 end	
+
+
+# Quick Sort
+
+def quick_sort(list)
+  return [] if list.empty?
+
+  groups = list.group_by { |n| n <=> list.first }
+
+  less_than    = groups[-1] || []
+  first        = groups[0]  || []
+  greater_than = groups[1]  || []
+
+  quick_sort(less_than) + first + quick_sort(greater_than)
+end
