953453ad81f073928dbd94912b652a8b1fa4bc26
Your Nameandrewyoo94 <andrewyoo.ny@gmail.com>
2021-08-23 20:42:00 -0400

    Dup Strings

--- datastruct_algos.rb
+++ datastruct_algos.rb
@@ -437,3 +437,22 @@ def quick_sort(list)
 
   quick_sort(less_than) + first + quick_sort(greater_than)
 end
+
+
+# String Duplicates
+
+def duplicates_in_str(str)
+  common = {}
+  ar = []
+
+  # create an array, put occurance at index of integer representation of char
+  str.each_char do |char|
+    ar[char.ord] = ar[char.ord].to_i + 1
+  end
+
+  # if value is greater than 1, convert index to char
+  (0..ar.length).each do |i|
+    common.push(i.chr) if !ar[i].nil? && ar[i] > 1
+  end
+  common.to_s
+end
