14cdb3a3d172f8c53106e0d8b56fb641a3eab3a0
Your Nameandrewyoo94 <andrewyoo.ny@gmail.com>
2021-08-25 23:09:00 -0400

    Merge Sort

--- datastruct_algos.rb
+++ datastruct_algos.rb
@@ -514,3 +514,47 @@ class Stack
     @head = nil
   end
 end
+
+
+
+# Merge Sort 
+
+def merge(left, right)
+  result = []
+  left_index, right_index = 0, 0
+  while (left_index < left.length && right_index < right.length)
+    left_element = left[left_index]
+    right_element = right[right_index]
+
+    if left_element <= right_element
+      result << left_element
+      left_index += 1
+    else
+      result << right_element
+      right_index += 1
+    end
+  end
+
+  # add whatever is left from the left or right to the result
+  if left
+    result += left[left_index..-1]
+  end
+  if right
+    result += right[right_index..-1]
+  end
+
+  return result
+end
+
+def merge_sort(m)
+  if m.length <= 1
+    return m
+  end
+
+  middle = m.length / 2
+  left = m[0...middle]
+  right = m[middle..-1]
+  left = merge_sort(left)
+  right = merge_sort(right)
+  return merge(left, right)
+end
