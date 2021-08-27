2597e979e98e66474cd967904f5838c058c1ed7d
Your Nameandrewyoo94 <andrewyoo.ny@gmail.com>
2021-08-26 21:37:33 -0400

    Reverse Queue

--- datastruct_algos.rb
+++ datastruct_algos.rb
@@ -558,3 +558,23 @@ def merge_sort(m)
   right = merge_sort(right)
   return merge(left, right)
 end
+
+
+
+# Reverse Queue
+
+def reverse(queue)
+	stack = []
+  
+	while !queue.empty? do
+		popped_item = queue.pop # Remove item from queue
+		stack.push(popped_item) # Insert removed item into stack
+	end
+
+	while !stack.empty? do
+		popped_item = stack.pop # Remove item from stack
+		queue.unshift(popped_item) # Insert removed item into queue
+	end
+
+	queue
+end
