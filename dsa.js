6fa29c76c17c019595a114e7ddc18eca1f6d6cf8
Your Nameandrewyoo94 <andrewyoo.ny@gmail.com>
2021-09-09 18:30:07 -0400

    Remove duplicates

--- dsa.js
+++ dsa.js
@@ -16,4 +16,23 @@ const findMaxMin = (arr) => {
       "max": max,
       "min": min
     };
+}
+
+// Remove Duplicates
+
+const removeDupes = (arr) => {
+  let result = [];
+  let previous = arr[0];
+  result[0] = previous;
+  
+  for(let i = 0; i < arr.length; i++) {
+    
+    if (arr[i] != previous) {
+      result.push(arr[i]);
+    }
+    
+    previous = arr[i];
   }
+  
+  return result;
+}
